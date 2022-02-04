79935cb1a30811a437e499fafe4b60cd91f82d91
yash-lol <yjoshi@ucsd.edu>
2022-02-04 13:46:47 -0800

    Update SkillDemoTest.java

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -5,6 +5,6 @@ import org.junit.Test;
 public class SkillDemoTest {
     @Test
     public void addition() {
-        assertEquals(5, SkillDemo.add(2, 4)); // failed test
+        assertEquals(5, SkillDemo.add(1, 4)); // failed test
     }
 }
